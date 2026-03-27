a6d2c5ca7f17be4bc55ee1267e25e7b24f942111
Tenjeeb K. C. <tenjeeb15@gmail.com>
2026-03-27 18:46:45 +0545

    Connected MongoDB for saving username and hashed password

--- auth-backend/server.js
+++ auth-backend/server.js
@@ -1,28 +1,46 @@
 const express = require('express');
+const bcrypt = require("bcrypt");
+const mongoose = require('mongoose')
+const User = require("./models/User")
+const dotenv = require("dotenv")
+
 const cors = require("cors");
 const app = express();
-const bcrypt = require("bcrypt");
-
 app.use(cors());
 app.use(express.json());   // Allows server to read json data from React
 
+const MongoURL = "mongodb+srv://tenjeeb15_db:auth-system-ok@cluster1.1czhxqa.mongodb.net/?appName=Cluster1"
 
-// We will build this next: The Login route
-app.post('/api/register', async(req, res) => {
-    const {email, password} = req.body;
+// Connect to MongoDB
+mongoose.connect(MongoURL)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.log("Connection error:", err))
+
+// Register Route
+app.post('/api/register', async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
     // Hash the password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    console.log(`Saving user: ${email} with Hash: ${hashedPassword}`);
+   // Create and Save th user
+   const newUser = new User({
+    email: email,
+    password: hashedPassword
+   })
 
-    // Next Step: Save this to a database!
-    res.status(201).send("User password hashed and ready to save!");
-})
+   const savedUser = await newUser.save();
+
+    res.status(201).json({message: "User saved to database!", userId: savedUser._id})
+  
+  }catch(err) {
+    res.status(500).json({error: "Failed to register user (Email might already exits)"})
+  }
+  });
 
 const port = 3000
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
 })
-  
